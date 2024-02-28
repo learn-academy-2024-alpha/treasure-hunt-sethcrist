c48bc38b886a2cf5fc66d6a91d872fe596844b2e
sethcrist <sethcrist50@gmail.com>
2024-02-28 09:35:17 -0800

    added 9 squares

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
+import Square from "./components/Square"
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -17,6 +18,11 @@ const App = () => {
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <div className="board">
+      {board.map((value, index) => {
+        return <Square />
+      })}
+      </div>
     </>
   )
 }
